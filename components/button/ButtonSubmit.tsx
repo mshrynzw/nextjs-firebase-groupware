4fbd215e4b069fd3270634cdf27185b1bc10ba4e
kaoruyuki <>
2024-08-30 01:55:48 +0900

    Fix Button

--- components/button/ButtonSubmit.tsx
+++ components/button/ButtonSubmit.tsx
@@ -6,7 +6,7 @@ const ButtonSubmit = ({ onClick }) => {
     <button
       type="submit"
       onClick={onClick}
-      className="rounded-md bg-gray-200 p-2 text-sm font-semibold shadow-xl duration-300 ease-in-out text-blueGray-600 hover:text-blueGray-200 hover:bg-blueGray-600 hover:-translate-y-1 hover:scale-110"
+      className="mr-1 mb-1 w-full rounded-xl bg-gray-200 p-2 px-6 py-3 text-sm font-bold uppercase shadow-xl outline-none duration-300 ease-in-out text-blueGray-600 bg-blueGray-800 hover:text-blueGray-200 hover:bg-blueGray-600 hover:-translate-y-1 hover:scale-110 focus:outline-none"
     >
       Submit
     </button>
