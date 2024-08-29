37c96fef3c3655a57b40bde87f02736f2bb3c706
kaoruyuki <>
2024-08-29 16:16:01 +0900

    Fix

--- components/app/info/Index.tsx
+++ components/app/info/Index.tsx
@@ -1,67 +1,66 @@
-"use client"; // クライアントコンポーネントとして定義
+"use client"
 
 import { usePathname } from "next/navigation"
-import { useRouter } from "next/router"; // useRouterをインポート
-import dynamic from "next/dynamic";
-import React, { useContext, useEffect, useState } from "react";
-import { AppContext } from "@/context/AppContext";
-import { motion, AnimatePresence } from "framer-motion";
+import dynamic from "next/dynamic"
+import React, { useContext, useEffect, useState } from "react"
+import { AppContext } from "@/context/AppContext"
+import { motion, AnimatePresence } from "framer-motion"
 
-const DynamicFind = dynamic(() => import("./Find"));
-const DynamicContainer = dynamic(() => import("@/components/container/Container"));
-const DynamicEdit = dynamic(() => import("./Edit"));
-const DynamicDelete = dynamic(() => import("./Delete"));
-const DynamicCreate = dynamic(() => import("./Create"));
-const DynamicButtonMenu = dynamic(() => import("@/components/button/ButtonMenu"));
+const DynamicFind = dynamic(() => import("./Find"))
+const DynamicContainer = dynamic(() => import("@/components/container/Container"))
+const DynamicEdit = dynamic(() => import("./Edit"))
+const DynamicDelete = dynamic(() => import("./Delete"))
+const DynamicCreate = dynamic(() => import("./Create"))
+const DynamicButtonMenu = dynamic(() => import("@/components/button/ButtonMenu"))
 
-const Index: React.FC = () => {
+const Index : React.FC = () => {
   const pathName = usePathname()
-  const { screen } = useContext(AppContext);
+  const { screen } = useContext(AppContext)
 
-  const [editInfo, setEditInfo] = useState(null);
-  const [deleteInfo, setDeleteInfo] = useState(null);
-  const [type, setType] = useState("plain");
-  const [isPreview, setIsPreview] = useState(false);
+  const [editInfo, setEditInfo] = useState(null)
+  const [deleteInfo, setDeleteInfo] = useState(null)
+  const [type, setType] = useState("plain")
+  const [isPreview, setIsPreview] = useState(false)
 
   useEffect(() => {
-    if (editInfo) setType(editInfo.type);
-  }, [editInfo]);
+    if (editInfo) setType(editInfo.type)
+  }, [editInfo])
 
-  const handleType = (str: string) => {
-    setType(str);
-  };
+  const handleType = (str : string) => {
+    setType(str)
+  }
 
   const handlePreview = () => {
-    setIsPreview(!isPreview);
-  };
+    setIsPreview(!isPreview)
+  }
 
   return (
     <>
       <AnimatePresence mode="wait">
         <motion.div
-          key={pathName} // router.routeを使用
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.5 }}
+          key={pathName}
+          initial={{ opacity : 0 }}
+          animate={{ opacity : 1 }}
+          exit={{ opacity : 0 }}
+          transition={{ duration : 0.5 }}
         >
-          <DynamicFind setEditInfo={setEditInfo} setDeleteInfo={setDeleteInfo} />
+          <DynamicFind setEditInfo={setEditInfo} setDeleteInfo={setDeleteInfo}/>
           {screen !== "find" && (
             <DynamicContainer>
               {screen === "edit" && editInfo !== null ? (
-                <DynamicEdit editInfo={editInfo} handlePreview={handlePreview} isPreview={isPreview} type={type} />
+                <DynamicEdit editInfo={editInfo} handlePreview={handlePreview} isPreview={isPreview} type={type}/>
               ) : screen === "delete" && deleteInfo !== null ? (
-                <DynamicDelete deleteInfo={deleteInfo} />
+                <DynamicDelete deleteInfo={deleteInfo}/>
               ) : screen === "create" ? (
-                <DynamicCreate type={type} isPreview={isPreview} handlePreview={handlePreview} />
+                <DynamicCreate type={type} isPreview={isPreview} handlePreview={handlePreview}/>
               ) : null}
             </DynamicContainer>
           )}
-          <DynamicButtonMenu handlePreview={handlePreview} handleType={handleType} isPreview={isPreview} type={type} />
+          <DynamicButtonMenu handlePreview={handlePreview} handleType={handleType} isPreview={isPreview} type={type}/>
         </motion.div>
       </AnimatePresence>
     </>
-  );
-};
+  )
+}
 
-export default Index;
+export default Index
