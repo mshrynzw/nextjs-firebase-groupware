4200de1eecd0c9c47f0ebf3ad2f97c8458b94c79
kaoruyuki <>
2024-09-08 13:26:56 +0900

    Fix

--- actions/app/setting/groupAction.ts
+++ actions/app/setting/groupAction.ts
@@ -5,7 +5,7 @@ import { User } from "@/types/user"
 
 export const createAction = async (formData : FormData) => {
   const title = formData.get("title") as string
-  const users = formData.get("users")as User[]
+  const users = JSON.parse(formData.get("users") as unknown as string) as User[]
 
   try {
     return await createdGroup(title, users)
@@ -16,7 +16,7 @@ export const createAction = async (formData : FormData) => {
 
 export const editAction = async (formData : FormData, id : string) => {
   const title = formData.get("title") as string
-  const users = formData.get("users") as User[]
+  const users = JSON.parse(formData.get("users") as unknown as string) as User[]
 
   try {
     return await editedGroup(id, title, users)
